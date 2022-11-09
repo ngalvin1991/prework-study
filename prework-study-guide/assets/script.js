d299755f22bec48058086de50ef1a6054039d46b
ngalvin1991 <117473760+ngalvin1991@users.noreply.github.com>
2022-11-09 16:44:45 +0000

    Control Flow Practice

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,17 +1,25 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-for(var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
-}
-/*
-if (topic === 'HTML') {
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+      console.log(topics[x]);
+    }
+  }
+
+function selectTopic() {
+if (topics[0] === 'HTML') {
 console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomtopic === 'CSS') {
  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (randomtopic === 'Git') {
  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (randomtopic === 'JavaScript') {
  console.log("Let's study JavaScript!");
 } else {
  console.log('Please try again!');
 }
-*/
+}
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
